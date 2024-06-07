becbff2ca68351065180ef06b67b95f4ac4446b8
Alirezakarimi <Km0fficial@yahoo.com>
2024-06-07 19:20:11 +0330

    run reace native asset

---
+++ android/app/src/main/assets/custom/index.ts
@@ -0,0 +1,4 @@
+export const AmazonPay = require('./amazonpay.png');
+export const ApplePay = require('./applepay.png')
+export const googlePay = require('./gpay.png')
+export const Avatar = require('./avatar.png')
