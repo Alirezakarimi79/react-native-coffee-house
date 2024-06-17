d20fcc4e64cdc5ba0c8c8308f5933825126c561a
Alirezakarimi <Km0fficial@yahoo.com>
2024-06-17 20:30:41 +0330

    hide splach screen

--- App.tsx
+++ App.tsx
@@ -1,15 +1,22 @@
-import {FC} from 'react';
+import {FC, useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import SplashScreen from 'react-native-splash-screen';
 
 import {DetailsScreen, PaymentScreen} from './src/screens';
+import {TabNavigator} from './src/navigators/tab_navigator';
 
 const Stack = createNativeStackNavigator();
 
 const App: FC = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name={'Tab'} component={TabNavigator} />
         <Stack.Screen
           name={'Details'}
           component={DetailsScreen}
